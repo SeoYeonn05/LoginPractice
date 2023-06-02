61193bbfea04a0c2517656c15f23c8bed493c41d
seoyeon <seoyeonn051@gmail.com>
2023-06-03 02:49:27 +0900

    fix: uid 추가

--- app/src/main/java/com/example/loginsignuppractice/model/User.kt
+++ app/src/main/java/com/example/loginsignuppractice/model/User.kt
@@ -1,7 +1,8 @@
 package com.example.loginsignuppractice.model
 
 data class UserInfo(
+    var uId: String,
     var friendsList: MutableList<String>? = null,
-    var nickname: String,
+    var chatRoomList: MutableList<String>? = null,
     var profileImage: Int
 )
